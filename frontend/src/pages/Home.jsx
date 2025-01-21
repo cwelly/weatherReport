bb937dd58f6fc05e9bac240f0ad3c2130cb3865b
cwelly <potato3884@naver.com>
2025-01-21 23:43:44 +0900

    feat little study about drag

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 
-import React from "react";
+import React, { useState } from "react";
+import { Droppable } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd"; 
+import Draggable from "react-draggable";
 function Home(){
-    return <>Home</>
-}
+  const cards = ["🂡", "🂢", "🂣", "🂤", "🂥"]; // 카드 내용
+    return (<> 
+    </>)
+} 
 export default Home;
